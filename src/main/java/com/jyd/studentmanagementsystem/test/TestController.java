688f5d44db2b72f179ba9741037d17fa87a45eb7
jayodkavinda <jayodkavinda@gmail.com>
2022-06-10 21:39:39 +0530

    add test controller

---
+++ src/main/java/com/jyd/studentmanagementsystem/test/TestController.java
@@ -0,0 +1,13 @@
+package com.jyd.studentmanagementsystem.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Student Management System";
+    }
+}
